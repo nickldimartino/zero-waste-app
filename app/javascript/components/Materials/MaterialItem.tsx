24b3b279febe83de413acc1e88be22f410360684
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-06-13 14:00:30 -0400

    Materials Page CSS

--- app/javascript/components/Materials/MaterialItem.tsx
+++ app/javascript/components/Materials/MaterialItem.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 function MaterialItem({ attributes }: { attributes: any }) {
   return (
-    <div className="card m-3" style={{"width":"18rem"}}>
+    <div className="card m-3 bg-info rounded" style={{"width":"18rem"}}>
       <div className="card-body d-flex flex-column justify-content-center">
-        <h3 className="card-title text-center">{attributes.name}</h3>
-        <Link to={`/materials/${attributes.slug}`} className="btn btn-primary">View Material</Link>
+        <div className="card-title text-center fs-2 text-light">{attributes.name}</div>
+        <Link to={`/materials/${attributes.slug}`} className="btn btn-outline-light border border-2 border-light btn-sm fs-4">View Material</Link>
       </div>
     </div>
   )
